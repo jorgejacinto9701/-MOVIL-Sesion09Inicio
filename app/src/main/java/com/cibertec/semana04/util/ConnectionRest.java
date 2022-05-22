8616120a65b61a7dae26761f80202935104a8cde
jorge jacinto gutarra <jorgejacinto9701@gmail.com>
2022-05-22 10:07:20 -0500

    Update ConnectionRest.java

--- app/src/main/java/com/cibertec/semana04/util/ConnectionRest.java
+++ app/src/main/java/com/cibertec/semana04/util/ConnectionRest.java
@@ -6,10 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ConnectionRest {
 
     public static final String URL = "https://api-cibertec-moviles.herokuapp.com/servicio/";
-    public static final String URL_PAIS = "https://restcountries.com/v3.1/";
+ 
 
     public static Retrofit retrofit = null;
-    public static Retrofit retrofitPais = null;
+    
     public static Retrofit getConnecion(){
         if (retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build();
@@ -17,11 +17,6 @@ public class ConnectionRest {
         return retrofit;
     }
 
-    public static Retrofit getConnecionRestPais(){
-        if (retrofitPais == null){
-            retrofitPais = new Retrofit.Builder().baseUrl(URL_PAIS).addConverterFactory(GsonConverterFactory.create()).build();
-        }
-        return retrofitPais;
-    }
+  
 
 }
